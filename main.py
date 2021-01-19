228035bf4d8d2f113d64da1f3479591598797d19
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2021-01-19 09:47:01 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -5,10 +5,14 @@
 #    else:
     #        light.clear()
 
+
 #while True:
     #    if input.light_level() > 8:
         #        music.ba_ding.play_until_done()
 
-music.play_melody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
 music.set_volume(20)
+
+music.play_melody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
 light.show_animation(light.rainbowAnimation, 500)
+
+
--- main.ts
+++ main.ts
@@ -7,6 +7,6 @@
 // while True:
 //     if input.light_level() > 8:
 //         music.ba_ding.play_until_done()
-music.playMelody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
 music.setVolume(20)
+music.playMelody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
 light.showAnimation(light.rainbowAnimation, 500)
