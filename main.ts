1da4e7198e697a3700a732ac66970cdc36658ca8
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2021-01-18 20:53:35 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+// while True:
+//     print("Sound level: " + input.sound_level())
+//     if input.sound_level() > 150:
+//         light.show_animation(light.rainbowAnimation, 500)
+//     else:
+//         light.clear()
+// while True:
+//     if input.light_level() > 8:
+//         music.ba_ding.play_until_done()
+music.playMelody("G F E E E F D D      C G F E E ", 150)
