5ee2ea1cbb1401e1b6fcb8c5b1dffe5964f29c32
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2021-01-20 16:37:28 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -139,7 +139,7 @@
 #light.clear()
 
 while True:
-    if input.sound_level() = 100:
+    if input.sound_level() > 100:
         light.set_all(light.rgb(204, 207, 68))
     else:
         light.clear()
--- main.ts
+++ main.ts
@@ -134,7 +134,7 @@
 // light.show_animation(light.rainbowAnimation, 500)
 // light.clear()
 while (true) {
-    if (input.soundLevel() >= 100) {
+    if (input.soundLevel() > 100) {
         light.setAll(light.rgb(204, 207, 68))
     } else {
         light.clear()
